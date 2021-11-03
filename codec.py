8ba7473ed476f5828226701dece73507f1dfa904
ArshZ0 <arshz3r0@gmail.com>
2021-11-03 02:09:35 +0100

    Get message and split binary ord chars

--- codec.py
+++ codec.py
@@ -7,6 +7,9 @@ import sys
 from PIL import Image
 import numpy as np
 
+def split_binary(binary):
+    return [binary[i:i+2] for i in range(0, len(binary), 2)]
+
 def main():
     args = sys.argv[1:]
     if (len(args)<3):
@@ -29,6 +32,12 @@ def main():
         exit()
 
     try:
+        message_file = open(args[1], "r")
+    except:
+        print("Error: \'"+args[1]+"\' does not exist")
+        exit()
+
+    try:
         output = open(args[2], "w+")
     except:
         print("Error: \'"+args[2]+"\' already exist")
@@ -43,7 +52,13 @@ def main():
             r,g,b,a = pixels[x, y]
             print("RED: ",r,"\tGREEN: ",g,"\tBLUE: ",b, "\tALPHA: ",a)
 
-
+    message = message_file.readline()
+    for i in range(len(message)-1):
+        binary = bin(ord(message[i]))[2:]
+        while (len(binary)<8):
+            binary = '0'+binary
+        split = split_binary(binary)
+        print(message[i],ord(message[i]),binary, split)
 
 if __name__ == '__main__' :
     main()
