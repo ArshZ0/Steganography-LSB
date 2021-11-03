e53cbe81c9e25fff8529af55c66804d065129a8f
ArshZ0 <arshz3r0@gmail.com>
2021-11-03 01:44:31 +0100

    Get pixels color RGBA

--- codec.py
+++ codec.py
@@ -1,11 +1,11 @@
-'''
 #########################
 # Author : ArshZ0       #
 # Created : 03 NOV 2021 #
 #########################
-'''
 
 import sys
+from PIL import Image
+import numpy as np
 
 def main():
     args = sys.argv[1:]
@@ -13,14 +13,36 @@ def main():
         print("Missing arguments\n\tpython codec.py <image> <message> <output>")
         exit()
     if (len(args)>3):
-        print("Too much arguments\n\tpython codec.py <image> <message> <output>")
+        print("Too many arguments\n\tpython codec.py <image> <message> <output>")
         exit()
 
     print('''
     #############################################################
     #   Steganography - Least Significant Bit Image - ArshZ0    #
-    #############################################################\n
+    #############################################################
     ''')
+
+    try: 
+        image = Image.open(args[0], "r")
+    except:
+        print("Error: \'"+args[0]+"\' does not exist")
+        exit()
+
+    try:
+        output = open(args[2], "w+")
+    except:
+        print("Error: \'"+args[2]+"\' already exist")
+        exit()
+
+
+    image = image.convert('RGBA')
+    pixels = image.load()
+
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
+            r,g,b,a = pixels[x, y]
+            print("RED: ",r,"\tGREEN: ",g,"\tBLUE: ",b, "\tALPHA: ",a)
+
 
 
 if __name__ == '__main__' :
