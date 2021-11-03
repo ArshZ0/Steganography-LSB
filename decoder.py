1c5048aa7b0e9220268c1d067d5c890ff47c2180
ArshZ0 <arshz3r0@gmail.com>
2021-11-03 03:37:55 +0100

    get output from image

--- decoder.py
+++ decoder.py
@@ -8,6 +8,12 @@ from PIL import Image
 import numpy as np
 from time import sleep
 
+def int_to_bin(val):
+    val = bin(val)[2:]
+    while (len(val)<8):
+        val = '0'+val
+    return val
+
 def main():
     args = sys.argv[1:]
     if (len(args)<2):
@@ -31,6 +37,9 @@ def main():
 
     print("Loading of "+args[0]+" ...\n");sleep(0.5)
 
+    image = image.convert('RGBA')
+    pixels = image.load()
+
     try:
         output = open(args[1], "x+")
     except:
@@ -38,6 +47,25 @@ def main():
         exit()
 
     print("Writing into "+args[1]+" ...\n");sleep(0.5)
+
+    message = ""
+
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
+            r,g,b,a = pixels[x, y]
+            r = int_to_bin(r)[-2:]
+            g = int_to_bin(g)[-2:]
+            b = int_to_bin(b)[-2:]
+            a = int_to_bin(a)[-2:]
+            rgba = int(r+g+b+a,2)
+            if (rgba!=0) :
+                message+=chr(rgba)
+    
+    output.write(message)
+    output.close()
+    image.close()
+
+
 
 if __name__ == '__main__' :
     main()
